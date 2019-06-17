47f2489882232e3b28e4ff13d4bb6c5cbf28efd6
Jonathan Kitzrow <jonkitzrow@gmail.com>
2019-06-16 23:33:23 -0500

    Game loop functional
    Movement works and obstacles both spawn and despawn as expected.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,15 +4,105 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    float moveCharge, actualScaleX, actualScaleY, spawnTimeNext;
+    public Transform arrow, arrowNext, player, obstacle;
+    int direction, directionNext, track;
+    public float chargeRate, spawnDelay;
+    float[] tracks;
+    bool[] tracksSpawn;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveCharge = 1f;
+        actualScaleX = arrow.localScale.x;
+        actualScaleY = arrow.localScale.y;
+        tracks = new float[4];
+        tracks[0] = -7f;
+        tracks[1] = -5f;
+        tracks[2] = -3f;
+        tracks[3] = -1f;
+        track = 1;
+        direction = 1;
+        directionNext = -1;
+        tracksSpawn = new bool[4];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // move step
+        if (Input.GetKeyDown("space") && moveCharge >= 1f)
+        {
+          // update track value
+          if (track + direction >= 0 && track + direction <= 3) // skip if the resulting value is out of bounds (should be impossible under normal operating parameters)
+          {
+            track += direction;
+            moveCharge = 0;
+          }
+          direction = directionNext;
+
+          // Generate the next direction with 50% chance of left or right
+          if (Random.Range(1, 3) == 1)
+          {
+            if (track + direction - 1 >= 0 && track + direction - 1 <= 3) // make sure this direction won't send the player off the tracks
+            {
+              directionNext = -1;
+            }
+            else
+            {
+              directionNext = 1;
+            }
+          }
+          else
+          {
+            if (track + direction + 1 >= 0 && track + direction + 1 <= 3) // make sure this direction won't send the player off the tracks
+            {
+              directionNext = 1;
+            }
+            else
+            {
+              directionNext = -1;
+            }
+          }
+        }
+
+        //update player position
+        player.position = new Vector3(tracks[track], -4f, 0);
+
+        // charge next move
+        if (moveCharge < 1)
+        {
+          moveCharge += chargeRate * Time.deltaTime;
+        }
+        else
+        {
+          moveCharge = 1;
+        }
+
+        // update UI readout
+        arrow.localScale = new Vector3(actualScaleX * moveCharge, actualScaleY * moveCharge * -direction, 1);
+        arrowNext.localScale = new Vector3(arrowNext.localScale.y, Mathf.Abs(arrowNext.localScale.y) * -directionNext, 1);
+
+        // Instantiate obstacles
+        if (Time.time >= spawnTimeNext)
+        {
+          spawnTimeNext = Time.time + spawnDelay;
+          for (int i = 0; i < 3; i++)
+          {
+            tracksSpawn[Random.Range(0, 4)] = true;
+          }
+          for (int i = 0; i < 4; i++)
+          {
+            if (tracksSpawn[i])
+            {
+              Instantiate(obstacle, new Vector3(tracks[i], 6f, -2f), Quaternion.Euler(0, 0, 0));
+            }
+          }
+          for (int i = 0; i < 4; i++)
+          {
+            tracksSpawn[i] = false;
+          }
+        }
     }
 }
--- Assets/Scripts/ObstacleMovement.cs
+++ Assets/Scripts/ObstacleMovement.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class ObstacleMovement : MonoBehaviour
 {
+    float speed = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += new Vector3(0, -1f, 0) * speed * Time.deltaTime;
+
+        if (transform.position.y <= -6f)
+        {
+          Destroy(this);
+        }
     }
 }
